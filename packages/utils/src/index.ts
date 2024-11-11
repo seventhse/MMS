cc35520105a281bb56b0bf4f9f0d9f8bda0f6c70
seventhse <season.sevent@icloud.com>
2024-11-11 17:09:36 +0800

    feat(utils): Add hooks
    1. Latest function hook
    2.On mount life cycle hook

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+export * from './hooks/use-latest'
+export * from './hooks/use-on-mount'
+
 export * from 'lodash-es'
 
 export function cn(...inputs: ClassValue[]) {
